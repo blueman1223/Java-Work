138d1061620abb8bef97592994e366b1fcacd026
blueman1223 <blueman1223@hotmail.com>
2021-05-24 01:00:31 +0800

    第三周-第三题作业提交

--- week03/netty-gateway/src/main/java/io/github/kimmking/gateway/filter/HeaderHttpRequestFilter.java
+++ week03/netty-gateway/src/main/java/io/github/kimmking/gateway/filter/HeaderHttpRequestFilter.java
@@ -3,10 +3,25 @@ package io.github.kimmking.gateway.filter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.FullHttpRequest;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class HeaderHttpRequestFilter implements HttpRequestFilter {
+    private String hostInfo;
+
+    public HeaderHttpRequestFilter() {
+        this.hostInfo = "UNKNOWN_HOST";
+        try {
+            InetAddress ia = InetAddress.getLocalHost();
+            this.hostInfo = ia.getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
-        fullRequest.headers().set("mao", "soul");
+        // 添加网关标识
+        fullRequest.headers().set("X-NETTY-GATEWAY", hostInfo);
     }
 }
